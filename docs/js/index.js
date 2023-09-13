e2d6494316889c17345f62c77a5ac4e4978aa740
Javier Andrés <55107141+javierandres-dev@users.noreply.github.com>
2023-09-12 21:45:41 -0500

    Update web page

---
+++ docs/js/index.js
@@ -0,0 +1,61 @@
+const devs = [
+  {
+    name: 'TATIANA  SANCHEZ HERNADEZ',
+    web: 'https://tatisanchezh.github.io/bit-00/',
+    github: 'https://github.com/Tatisanchezh',
+  },
+  {
+    name: 'SERGIO ANDRÉS VILLAMARIN LÓPEZ',
+    web: 'https://sergiovilla07.github.io/bit-00/',
+    github: 'https://github.com/Sergiovilla07?tab=repositories',
+  },
+  {
+    name: 'SARA MILENA SOTAQUIRA PINEDA',
+    web: 'https://allenguitar.github.io/bit-00/',
+    github: 'https://github.com/AllenGuitar',
+  },
+  {
+    name: 'CAMILO ANDRES MARQUEZ AGUDELO',
+    web: 'https://andresmot24.github.io/bit-repo_00/',
+    github: 'https://github.com/andresmot24',
+  },
+  {
+    name: 'MATEO  LONDOÑO RIVADENEIRA',
+    web: 'https://mlondonoriv.github.io/bit-00/',
+    github: 'https://github.com/Mlondonoriv',
+  },
+  { name: 'DARLEIDA  PRADA DEVIA', web: null, github: null },
+  { name: 'JOSE DAVID MORENO MORENO', web: null, github: null },
+  {
+    name: 'LUISA ALEJANDRA RODRÍGUEZ MORENO',
+    web: 'https://luisa-rodriguez1991.github.io/bit-00/',
+    github: ' luisa-rodriguez1991',
+  },
+  {
+    name: 'HECTOR LUIS MEDRANO SANCHEZ',
+    web: 'https://hmedran0.github.io/bit-00/',
+    github: 'https://github.com/HMedran0',
+  },
+  { name: 'NATALI  GÓMEZ URRUTIA', web: null, github: null },
+  {
+    name: 'RAUL SANTIAGO RAMIREZ PUYO',
+    web: 'https://github.com/Designedby-Radol/git-00',
+    github: 'https://github.com/Designedby-Radol',
+  },
+  {
+    name: 'NELSON MAURICIO CARVAJAL',
+    web: 'https://cronox7.github.io/bit-00/',
+    github: 'https://github.com/CronoX7',
+  },
+  {
+    name: 'GUILLERMO LUCIANO',
+    web: 'https://guillermoabril.github.io/bit-00/',
+    github: 'https://github.com/Guillermoabril',
+  },
+  {
+    name: 'HERNANDO JOSÉ MONTIEL REYES',
+    web: 'https://josereyes05.github.io/bit-00/',
+    github: 'https://github.com/josereyes05',
+  },
+  { name: 'LILIANA ANDREA MOLINA FRANCO', web: null, github: null },
+];
