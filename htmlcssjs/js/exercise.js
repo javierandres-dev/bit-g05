fb4a0fa2869429354a1a63b4d62b680c6729a93f
Javier Andrés <55107141+javierandres-dev@users.noreply.github.com>
2023-09-20 22:10:28 -0500

    update htmlcssjs directory

--- htmlcssjs/js/exercise.js
+++ htmlcssjs/js/exercise.js
@@ -1,5 +1,5 @@
 'use strict';
-const d = document;
+/* const d = document;
 const $form = d.querySelector('form');
 const $p = d.querySelector('p');
 
@@ -27,4 +27,33 @@ function saludar(name) {
       $p.textContent = '';
     }, 3000);
   }
-}
+} */
+const d = document,
+  $root = d.getElementById('root'),
+  $loader = d.getElementById('loader');
+
+let html = '';
+
+const showUsers = (users) => {
+  for (let i = 0; i < users.length; i++) {
+    html += `<li>${users[i].name}</li>`;
+  }
+};
+
+(async () => {
+  try {
+    $loader.classList.remove('d-none');
+    $loader.classList.add('loader');
+    let res = await fetch('https://jsonplaceholder.typicode.com/users');
+    res = await res.json();
+    showUsers(res);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    setTimeout(() => {
+      $loader.classList.remove('loader');
+      $loader.classList.add('d-none');
+      $root.innerHTML = html;
+    }, 4000);
+  }
+})();
