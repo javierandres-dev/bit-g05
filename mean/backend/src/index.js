4bc1fb4be89cd37e4af415d8a23ca558d9936c56
Javier Andrés <55107141+javierandres-dev@users.noreply.github.com>
2023-09-26 19:07:47 -0500

    mean

--- mean/backend/src/index.js
+++ mean/backend/src/index.js
@@ -1,1 +1,9 @@
+// Entry point, punto de entrada de mi app
+require('./database');
 const server = require('./server');
+
+const port = server.get('port');
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
--- mean/backend/src/server.js
+++ mean/backend/src/server.js
@@ -4,14 +4,12 @@ const morgan = require('morgan');
 const server = express();
 const port = 4000;
 
+server.set('port', port);
+
 server.use(morgan('dev'));
 
 server.get('/', (request, response) => {
   response.send('works!');
-});
-
-server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
 });
 
 module.exports = server;
